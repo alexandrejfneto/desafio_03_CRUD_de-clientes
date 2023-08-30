8651999b08a96c20537bc56a35930895bef3503d
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-08-30 16:50:30 -0300

    Create Project, classes Cliente and ClienteDTO

---
+++ src/main/java/com/alejfneto/desafio_03/dto/ClientDTO.java
@@ -0,0 +1,53 @@
+package com.alejfneto.desafio_03.dto;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private LocalDate birthDate;
+	private Integer children;
+	
+	public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	@Override
+	public String toString() {
+		return "ClientDTO [id=" + id + ", name=" + name + ", cpf=" + cpf + ", income=" + income + ", birthDate="
+				+ birthDate + ", children=" + children + "]";
+	}
+	
+}
