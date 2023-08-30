d520482ead2e94c4430014cf67484fc5ca4c30c7
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-08-30 20:49:30 -0300

    insert and update implementation

--- src/main/java/com/alejfneto/desafio_03/controlers/ClientControler.java
+++ src/main/java/com/alejfneto/desafio_03/controlers/ClientControler.java
@@ -1,14 +1,24 @@
 package com.alejfneto.desafio_03.controlers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alejfneto.desafio_03.dto.ClientDTO;
 import com.alejfneto.desafio_03.services.ClientService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping (value = "/clients")
@@ -23,5 +33,26 @@ public class ClientControler {
 		return ResponseEntity.ok(clientDTO);		
 	}
 	
+	@GetMapping
+	public ResponseEntity<Page <ClientDTO>> findAll (Pageable pageable){
+		Page <ClientDTO> clientDTO = service.findAll(pageable);
+		return ResponseEntity.ok(clientDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO clientDTO) {
+		clientDTO = service.insert(clientDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(clientDTO.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(clientDTO);
+	}
+	
+	@PutMapping (value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@Valid @RequestBody ClientDTO clientDTO, @PathVariable Long id) {
+		clientDTO = service.update(id, clientDTO);
+		return ResponseEntity.ok(clientDTO);
+	}
 
 }
--- src/main/java/com/alejfneto/desafio_03/services/ClientService.java
+++ src/main/java/com/alejfneto/desafio_03/services/ClientService.java
@@ -3,6 +3,8 @@ package com.alejfneto.desafio_03.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +12,8 @@ import com.alejfneto.desafio_03.dto.ClientDTO;
 import com.alejfneto.desafio_03.entities.Client;
 import com.alejfneto.desafio_03.repositories.ClientRepository;
 import com.alejfneto.desafio_03.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -23,6 +27,37 @@ public class ClientService {
 		Client client = opt.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado!"));
 		ClientDTO cltDTO = new ClientDTO(client);
 		return cltDTO;
+	}
+	
+	@Transactional (readOnly = true)
+	public Page <ClientDTO> findAll(Pageable pageable) {
+		Page <Client> listResult = repository.findAll(pageable);
+		Page <ClientDTO> listResultFinal = listResult.map(x -> new ClientDTO(x));
+		return listResultFinal;		
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO clientDTO) {
+		//Metodo Alternativo com instanciacao direta no construtor
+		Client client = new Client (null, clientDTO.getName(), clientDTO.getCpf(), clientDTO.getIncome(), clientDTO.getBirthDate(), clientDTO.getChildren());
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO clientDTO) {
+		try {
+		Client clt = repository.getReferenceById(id);
+		clt.setName(clientDTO.getName());
+		clt.setCpf(clientDTO.getCpf());
+		clt.setIncome(clientDTO.getIncome());
+		clt.setBirthDate(clientDTO.getBirthDate());
+		clt.setChildren(clientDTO.getChildren());
+		clt = repository.save(clt);
+		return new ClientDTO(clt);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException("Recurso não encontrado!");			
+		}
 	}
 
 }
